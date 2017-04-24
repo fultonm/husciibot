a714fbaae4c45664de1be37fa77f31b2f14d8bc5
Beebkips <conorm413@gmail.com>
2017-04-23 22:27:25 -0700

    Create InputTester.py

---
+++ InputTester.py
@@ -0,0 +1,17 @@
+"""
+Tester file for input
+"""
+
+from <class file> import <class file>
+
+<var> = <class file>();
+user = <username>
+channel = <channel>
+
+if __name__ == "__main__":
+    while(1):
+        command = input("~*")
+        print("-=System=-")
+        response = <var>.<method>(command, channel, user);
+        print("-=\end=-")
+        print(response)
